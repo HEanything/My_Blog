64dab0d2cc08890d6fd9585cbd988bca05ee09db
HEanything <3434532148@qq.com>
2024-12-16 14:47:38 +0800

    修改了可以根据标签名设置标签

--- src/main/java/com/example/myblog/controller/ArticleLabelController.java
+++ src/main/java/com/example/myblog/controller/ArticleLabelController.java
@@ -1,9 +1,10 @@
 package com.example.myblog.controller;
 
 import com.example.myblog.pojo.Result;
+import com.example.myblog.pojo2.BlogLabel;
 import com.example.myblog.pojo2.BlogSetArticleLabel;
 import com.example.myblog.service.ArticleLabelService;
-import com.example.myblog.service.ArticleService;
+import com.example.myblog.service.LabelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,7 +15,9 @@ import java.util.Map;
 public class ArticleLabelController {
     @Autowired
     private ArticleLabelService articleLabelService;
-    //给文章设置标签
+    @Autowired
+    private LabelService LabelService;
+    //给文章设置标签///目前是根据标签id设置
     //为文章设置标签（更新）
     //若没有标签就设置标签，有就更新
     @PostMapping("/api/Articles/{articleId}/setLabels")
@@ -36,6 +39,35 @@ public class ArticleLabelController {
         } catch (Exception e) {
             e.printStackTrace();
             return Result.error("设置标签失败");
+        }
+    }
+    //////////////根据标签名设置标签/////
+    @PostMapping("/api/Articles/{articleId}/setLabelsByName")
+    public Result setLabelsByName(@PathVariable int articleId, @RequestBody Map<String, String> params) {
+        String labelName=params.get("labelName");
+        ////根据标签名查找标签id
+        BlogLabel blogLabel=LabelService.getLabelByName(labelName);
+        if (blogLabel==null){
+            return Result.error("标签不存在");
+        }else{
+            String labelId=String.valueOf(blogLabel.getLabelId());
+            BlogSetArticleLabel blogSetArticleLabel=articleLabelService.getLabelByArticleId(articleId);
+            if (blogSetArticleLabel!=null){
+                try {
+                    articleLabelService.updateLabels(articleId, labelId);
+                    return Result.success();
+                }catch (Exception e){
+                    e.printStackTrace();
+                    return Result.error("设置标签失败");
+                }
+            }
+            try {
+                articleLabelService.setLabels(articleId, labelId);
+                return Result.success();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return Result.error("设置标签失败");
+            }
         }
     }
 
